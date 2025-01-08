6168d6b47be578c58dcf9b24ee5dd04fc6ffdb43
Jean_Land <Jeanmiranda1255@gmail.com>
2025-01-08 02:09:00 -0300

    Update jogonumerosecreto by jean.js

--- jogonumerosecreto by jean.js
+++ jogonumerosecreto by jean.js
@@ -6,9 +6,17 @@ let tentativas = 0;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
-function exibirTextoInicial(){
+function exibirTextoInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
@@ -39,7 +47,6 @@ function gerarNumeroAleatorio() {
     let quantidadeDeNumerosSorteados = listaDeNumerosSorteados.length;
     if (quantidadeDeNumerosSorteados === numeroLimite) {
         listaDeNumerosSorteados = [];
-        
     }
     if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
         return gerarNumeroAleatorio();
@@ -60,8 +67,19 @@ function reiniciarJogo() {
     limparCampo();
     tentativas = 0;
     exibirTextoInicial();
-    document.getElementById('reiniciar').setAttribute('disabled', 'true'); 
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
 }
 
 // Exibindo o texto inicial ao carregar a página
 exibirTextoInicial();
+
+
+
+
+
+
+
+
+
+
+
